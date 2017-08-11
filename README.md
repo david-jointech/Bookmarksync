b694311c24f92aca6a2579fd2e5dae345ad42fb1
David Magnus Henriques <david.magnus@acagamics.de>
2017-08-11 21:42:32 +0200

    fixed unit test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://drone.eorlbruder.de/api/badges/EorlBruder/Bookmarksync/status.svg)](https://drone.eorlbruder.de/EorlBruder/Bookmarksync)
+
 A kotlin-JVM tool for syncing Wallabag to Shaarli. 
 
 More Documentation to follow
--- src/test/kotlin/de/eorlbruder/bookmarksync/core/utils/MergeUtilsTest.kt
+++ src/test/kotlin/de/eorlbruder/bookmarksync/core/utils/MergeUtilsTest.kt
@@ -12,11 +12,11 @@ class MergeUtilsTest {
         val targetTags = HashSet<String>()
         targetTags.add("Art")
         targetTags.add("Wallabag")
-        assertTrue(MergeUtils.haveTagsChanged(sourceTags, targetTags))
-        targetTags.add("Blafoo")
-        assertTrue(MergeUtils.haveTagsChanged(sourceTags, targetTags))
-        sourceTags.add("NewTag")
         assertFalse(MergeUtils.haveTagsChanged(sourceTags, targetTags))
+        targetTags.add("Blafoo")
+        assertFalse(MergeUtils.haveTagsChanged(sourceTags, targetTags))
+        sourceTags.add("NewTag")
+        assertTrue(MergeUtils.haveTagsChanged(sourceTags, targetTags))
     }
 
     @Test
